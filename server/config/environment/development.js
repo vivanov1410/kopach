523114c64f5b04013f96255233ab5560f69d5031
Viacheslav Ivanov <slava.eth@gmail.com>
2015-03-14 21:37:18 -0700

    added basic project structure

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://admin:admin@dogen.mongohq.com:10005/kopach-dev'
+  },
+
+  seedDB: true
+};
